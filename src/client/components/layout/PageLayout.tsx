e1dc0affa145be84f91d059741aa788cebf0d1db
Adam Lane <enalmada@gmail.com>
2023-07-04 19:29:55 -0700

    feat: testing mantine

--- src/client/components/layout/PageLayout.tsx
+++ src/client/components/layout/PageLayout.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function PageLayout(props: Props) {
   return (
     <Layout>
-      <section className="page-class border-b bg-white py-8 text-black">
+      <section className="page-class border-b bg-slate-100 py-8 text-black">
         <div className="container mx-auto px-4">{props.children}</div>
       </section>
     </Layout>
--- src/client/components/tasks/Task.tsx
+++ src/client/components/tasks/Task.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 // import ReactTooltip from "react-tooltip";
 import { TaskStatus, type Task, type TaskInput } from "@/client/gql/graphql";
-import { TASKS, UPSERT_TASK } from "@/client/queries-mutations";
+import { UPSERT_TASK } from "@/client/queries-mutations";
 import { formatRelativeDate } from "@/client/utils/date";
 import { useMutation } from "@apollo/client";
 import { format } from "date-fns";
+import { Text, Paper } from '@mantine/core';
+import { Checkbox, Container } from '@mantine/core';
 
 interface Props {
   task: Task;
@@ -19,9 +21,7 @@ const Task = (props: Props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [clientSide, setClientSide] = useState(false);
 
-  const [upsertTask] = useMutation(UPSERT_TASK, {
-    refetchQueries: [{ query: TASKS }],
-  });
+  const [upsertTask] = useMutation(UPSERT_TASK);
 
   useEffect(() => {
     // update some client side state to say it is now safe to render the client-side only component
@@ -29,6 +29,37 @@ const Task = (props: Props) => {
   }, []);
 
   return (
+      <>
+      <Container size="sm" className={"mb-3"}>
+
+        <Paper shadow="xs" p="xl">
+
+          <Checkbox
+              label={title}
+              size="lg"
+              defaultChecked={status === "COMPLETED"}
+              onClick={event => event.stopPropagation()}
+              onChange={(e) => {
+                const input: TaskInput = {
+                  id: id,
+                  title: title,
+                  description: description,
+                  dueDate: dueDate ? dueDate : undefined,
+                  status: status === "ACTIVE" ? TaskStatus.Completed : TaskStatus.Active,
+                };
+
+                try {
+                  // use void to skip await in a void callback
+                  // https://github.com/typescript-eslint/typescript-eslint/issues/4619#issuecomment-1055614155
+                  void upsertTask({ variables: { input } });
+                } catch (e) {
+                  //
+                }
+              }}
+          />
+
+        </Paper>
+      </Container>
     <div className="mb-3 flex max-w-md rounded-lg bg-white shadow-lg md:mx-auto md:max-w-2xl ">
       <div className="flex w-full items-start px-4 py-6">
         <div>
@@ -80,6 +111,7 @@ const Task = (props: Props) => {
         </div>
       </div>
     </div>
+        </>
   );
 };
 
