71001f782e806f046400da430c9abef5cd20f192
Adam Lane <enalmada@gmail.com>
2023-07-30 23:03:31 -0700

    fix: cloudflare pages edge

--- next.config.mjs
+++ next.config.mjs
@@ -94,6 +94,20 @@ const config = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'robohash.org'],
   },
+  webpack: (config) => {
+    // Remove kysely server deps.
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      path: false,
+      fs: false,
+      net: false,
+      tls: false,
+      dns: false,
+      stream: false,
+    };
+
+    return config;
+  },
 };
 
 // @ts-ignore
