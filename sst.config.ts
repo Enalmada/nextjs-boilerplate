69b235c4dfb1ee3508ff8ecfb7b698bcd90ff690
Adam Lane <enalmada@gmail.com>
2023-08-10 10:54:08 -0700

    feat: sst initial

---
+++ sst.config.ts
@@ -0,0 +1,36 @@
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
+
+export default {
+  config(input) {
+    let profileName;
+    switch (input.stage) {
+      case 'production':
+        profileName = 'lbc-production';
+        break;
+      case 'staging':
+        profileName = 'lbc-staging';
+        break;
+      case 'dev':
+        profileName = 'lbc-dev';
+        break;
+      default:
+        profileName = 'lbc-staging';
+    }
+
+    return {
+      name: 't3-challenge',
+      region: 'us-east-1',
+      profile: profileName,
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, 'site');
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
