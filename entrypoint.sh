09fe1ecff218c7fd1b1a0567e189df05da142bdd
Adam Lane <enalmada@gmail.com>
2023-09-19 11:35:30 -0700

    migrate on cloud run

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 printenv > /app/.env.production.local
 
 # Run DB migrations (db url is currently runtime parameter)
-bun run drizzle:migrate
+bun run drizzle:migrate:prod
 
 # Start Next.js server bound to all network interfaces
 # If you use custom server.js, make sure it binds to 0.0.0.0
