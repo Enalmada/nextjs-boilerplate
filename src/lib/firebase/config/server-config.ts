4b5264602ca90562ab0d5a5a542119eba88be9e5
Adam Lane <enalmada@gmail.com>
2023-09-19 10:20:35 -0700

    docker to bun

--- src/lib/firebase/config/server-config.ts
+++ src/lib/firebase/config/server-config.ts
@@ -4,7 +4,7 @@ export const serverConfig = {
   serviceAccount: {
     projectId: process.env.FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
   },
 };
 
