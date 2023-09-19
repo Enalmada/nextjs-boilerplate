dc199d6b91cd4e27bb2a0f8aee6bbe1326191b9c
Adam Lane <enalmada@gmail.com>
2023-09-19 11:20:08 -0700

    docker to bun

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Create .env.production.local with all environment variables
 printenv > /app/.env.production.local
 
+# Run DB migrations (db url is currently runtime parameter)
+bun run drizzle:migrate
+
 # Start Next.js server bound to all network interfaces
 # If you use custom server.js, make sure it binds to 0.0.0.0
 HOST=0.0.0.0 exec node server.js
