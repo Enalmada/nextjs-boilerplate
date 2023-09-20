83ef54544af4b19aa0aafb7b39960af1d1883fb3
Adam Lane <enalmada@gmail.com>
2023-09-19 18:54:14 -0700

    bun tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'pnpm run dev:only',
+    command: 'bun run dev:only',
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
