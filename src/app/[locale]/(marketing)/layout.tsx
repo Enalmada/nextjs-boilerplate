7d89c0c938c713ebe20cb3aa73e78ed38f1694b6
Adam Lane <enalmada@gmail.com>
2023-09-26 15:21:39 -0700

    cloudflare bulid

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,10 +1,5 @@
 import { MarketingLayout } from '@/app/[locale]/(marketing)/MarketingLayout';
 
-// Editor may say this error but tsc doesn't
-// TS71003: "process.env.NEXT_RUNTIME_EDGE ? 'edge' : 'nodejs'" is not a valid value for the "runtime" option. The configuration must be statically analyzable.
-// This may not be imported from another file
-export const runtime = 'edge';
-
 // Using the searchParams Pages prop will opt the page into dynamic rendering at request time.
 // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering#dynamic-functions
 
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,6 +9,8 @@ import metadataConfig from '@/metadata.config';
 import clsx from 'clsx';
 import { NextIntlClientProvider, type AbstractIntlMessages } from 'next-intl';
 
+export const runtime = 'edge';
+
 type Props = {
   children: React.ReactNode;
   params?: {
