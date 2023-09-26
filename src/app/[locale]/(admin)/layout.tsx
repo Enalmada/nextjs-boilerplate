f5a807ebe2635e92ac74e3f8476812bc1548da85
Adam Lane <enalmada@gmail.com>
2023-09-26 14:59:48 -0700

    cloudflare bulid

--- src/app/[locale]/(admin)/layout.tsx
+++ src/app/[locale]/(admin)/layout.tsx
@@ -9,7 +9,7 @@ import { useSuspenseQuery } from '@apollo/client';
 // Editor may say this error but tsc doesn't
 // TS71003: "process.env.NEXT_RUNTIME_EDGE ? 'edge' : 'nodejs'" is not a valid value for the "runtime" option. The configuration must be statically analyzable.
 // This may not be imported from another file
-// export const runtime = 'edge';
+export const runtime = 'edge';
 
 export const dynamic = 'force-dynamic';
 
