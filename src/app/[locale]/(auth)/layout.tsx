6d8cffd4600a9a9a97c8c6ef1ee8114b42eacd77
Adam Lane <enalmada@gmail.com>
2023-09-26 15:09:45 -0700

    cloudflare bulid

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,5 +1,10 @@
 import AuthLayout from '@/app/[locale]/(auth)/AuthLayout';
 
+// Editor may say this error but tsc doesn't
+// TS71003: "process.env.NEXT_RUNTIME_EDGE ? 'edge' : 'nodejs'" is not a valid value for the "runtime" option. The configuration must be statically analyzable.
+// This may not be imported from another file
+export const runtime = 'edge';
+
 // Using the searchParams Pages prop will opt the page into dynamic rendering at request time.
 // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering#dynamic-functions
 export const dynamic = 'force-static';
