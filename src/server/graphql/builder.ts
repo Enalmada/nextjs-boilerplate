cd41ee627ca92a28ee4de468b5a9773b15b4e81d
Adam Lane <enalmada@gmail.com>
2023-10-25 14:11:38 -0700

    pothos

--- src/server/graphql/builder.ts
+++ src/server/graphql/builder.ts
@@ -1,27 +1,13 @@
-// https://pothos-graphql.dev/docs/plugins/prisma
-// https://www.prisma.io/blog/e2e-type-safety-graphql-react-3-fbV2ZVIGWg#define-a-date-scalar-type
+import { type User } from '@/server/db/schema';
+import { initializeBuilder } from '@enalmada/next-gql/server';
 import SchemaBuilder from '@pothos/core';
 import WithInputPlugin from '@pothos/plugin-with-input';
-import { DateTimeResolver, JSONResolver, NonEmptyStringResolver } from 'graphql-scalars';
 
-import { type MyContextType } from './server';
+export interface MyContextType {
+  currentUser: User;
+}
 
-// Complexity taken care of by armor. Use here if not there
-// https://escape.tech/graphql-armor/
-// https://pothos-graphql.dev/docs/plugins/complexity
-/*
-const complexity = {
-  defaultComplexity: 1,
-  defaultListMultiplier: 10,
-  limit: {
-    complexity: 500,
-    depth: 10,
-    breadth: 50,
-  },
-};
- */
-
-export interface SchemaTypes {
+export interface DefaultSchemaTypes {
   Scalars: {
     DateTime: {
       Input: Date;
@@ -39,49 +25,8 @@ export interface SchemaTypes {
   Context: MyContextType;
 }
 
-export type TypesWithDefault = PothosSchemaTypes.ExtendDefaultTypes<SchemaTypes>;
-
-export const builder = new SchemaBuilder<SchemaTypes>({
+export const builder = new SchemaBuilder<DefaultSchemaTypes>({
   plugins: [WithInputPlugin],
-  /*
-  errorOptions: {
-    directResult: true,
-    defaultTypes: [Error],
-  },
-   */
 });
 
-/*
-const ErrorInterface = builder.interfaceRef<Error>('Error').implement({
-  fields: (t) => ({
-    message: t.exposeString('message'),
-  }),
-});
-
-builder.objectType(Error, {
-  name: 'BaseError',
-  interfaces: [ErrorInterface],
-});
-
-builder.objectType(UnauthorizedError, {
-  name: 'UnauthorizedError',
-  interfaces: [ErrorInterface],
-});
-
-builder.objectType(NotFoundError, {
-  name: 'NotFoundError',
-  interfaces: [ErrorInterface],
-});
- */
-
-builder.addScalarType('DateTime', DateTimeResolver, {});
-builder.addScalarType('JSON', JSONResolver, {});
-builder.addScalarType('NonEmptyString', NonEmptyStringResolver, {});
-
-builder.queryType({
-  description: 'The query root type.',
-});
-
-builder.mutationType({
-  description: 'The query mutation type.',
-});
+initializeBuilder(builder);
