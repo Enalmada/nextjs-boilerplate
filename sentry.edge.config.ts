be4ce57f76b2d1c95ba3ccffccc4c93373a25227
Adam Lane <enalmada@gmail.com>
2024-11-02 12:50:19 -0700

    sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,28 +6,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-	dsn: process.env.SENTRY_DSN,
+	dsn: "https://e827c97a46cc34fdcf55b79c6a3fe773@o32548.ingest.us.sentry.io/4505625265438720",
 
-	// Adjust this value in production, or use tracesSampler for greater control
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
 	tracesSampleRate: 1,
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
-
-	environment: process.env.NEXT_PUBLIC_APP_ENV,
-
-	beforeSend: (event, hint) => {
-		// https://github.com/getsentry/sentry-javascript/issues/1600
-		console.error(hint.originalException || hint.syntheticException); // eslint-disable-line no-console
-		if (process.env.NODE_ENV === "test") {
-			return null; // this drops the event and nothing will be send to sentry
-		}
-
-		// keep this line separate to comment it out easily locally to watch errors
-		if (process.env.NODE_ENV === "development") {
-			return null; // this drops the event and nothing will be send to sentry
-		}
-
-		return event;
-	},
 });
